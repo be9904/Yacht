70d0d683390329810e041529464ec57d0af0a879
be9904 <be9904@gmail.com>
2022-01-02 01:15:15 +0900

    Code Cleanup

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    public static Vector3 sideOne = new Vector3(-90, 0, 0);
-    public static Vector3 sideTwo = new Vector3(0, 0, -90);
+    public static Vector3 sideOne   = new Vector3(-90, 0, 0);
+    public static Vector3 sideTwo   = new Vector3(0, 0, -90);
     public static Vector3 sideThree = new Vector3(0, 0, 0);
-    public static Vector3 sideFour = new Vector3(0, 0, -180);
-    public static Vector3 sideFive = new Vector3(0, 0, 90);
-    public static Vector3 sideSix = new Vector3(90, 0, 0);
+    public static Vector3 sideFour  = new Vector3(0, 0, -180);
+    public static Vector3 sideFive  = new Vector3(0, 0, 90);
+    public static Vector3 sideSix   = new Vector3(90, 0, 0);
 
-    public int m_state;     // 0: Waiting, 1: Rolled
+    // Public Properties
+    public int m_state;     // 0: Waiting,    1: Rolled
+    public int m_chosen;    // 0: Not Chosen, 1: Chosen
+    
+    float m_rotSpeed;
     Vector3 m_initialPos;
     Rigidbody diceRigidbody;
-    float m_rotSpeed;
-
+    
     public Roll roll;
     // Start is called before the first frame update
     void Start()
     {
-        m_rotSpeed = 150f;  m_state = 0;
+        m_chosen = 0;   m_rotSpeed = 150f;  m_state = 0;
         diceRigidbody = GetComponent<Rigidbody>();
 
         m_initialPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
